49e57f5c94c091a61dc840072752042ff43666e8
PolarsBear <63355054+PolarsBear@users.noreply.github.com>
2022-03-02 22:00:28 -0300

    Replace audiofile with librosa

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=["xmltodict","Pillow", "audiofile"],
+    install_requires=["xmltodict","Pillow", "librosa"],
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
